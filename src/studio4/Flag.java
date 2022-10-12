da74068a7110f762e5b6b88967901790f8bc97fb
carolinapedraza <carolinapedraza@localhost>
2022-10-12 17:00:03 -0500

    :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,38 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		//background
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.9, 0.4);
 		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.rectangle(0.5, 0.5, 0.9, 0.4);
+		
+		//left column
+		StdDraw.setPenColor(2, 4, 10);
+		StdDraw.filledRectangle(0.2, 0.85, 0.3, 0.05);
+		
+		StdDraw.setPenColor(200, 200, 200);
+		StdDraw.filledRectangle(0.2, 0.65, 0.3, 0.15);
+		
+		StdDraw.setPenColor(200, 0, 200);
+		StdDraw.filledRectangle(0.2, 0.45, 0.3, 0.05);
+		
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledRectangle(0.2, 0.3, 0.3, 0.1);
+		
+		StdDraw.setPenColor(0, 0, 200);
+		StdDraw.filledRectangle(0.2, 0.15, 0.3, 0.05);
+		
+		//right column
+		StdDraw.setPenColor(0, 200, 200);
+		StdDraw.filledRectangle(0.8, 0.8, 0.3, 0.1);
+		
+		StdDraw.setPenColor(255, 100, 0);
+		StdDraw.filledRectangle(0.8, 0.5, 0.3, 0.2);
+		
+		StdDraw.setPenColor(255, 200, 255);
+		StdDraw.filledRectangle(0.8, 0.25, 0.3, 0.05);
 	}
 }
---
+++ src/studio4/Italy.java
@@ -0,0 +1,17 @@
+package studio4;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Italy {
+	public static void main(String[] args) {
+	
+	StdDraw.setPenColor(255, 255, 255);
+	StdDraw.filledRectangle(0.5, 0.5, 0.9, 0.3);
+	
+	StdDraw.setPenColor(0, 0, 255);
+	StdDraw.filledRectangle(0.175, 0.5, 0.15, 0.3);
+	
+	StdDraw.setPenColor(255, 0, 0);
+	StdDraw.filledRectangle(0.825, 0.5, 0.15, 0.3);
+	}
+}
